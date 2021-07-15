b14aad73da82c6ce0e9f378aed5282a52ea50e97
safecorners <safeconers.mobile@gmail.com>
2021-07-15 11:38:05 +0900

    [#5] AccountMapper 인터페이스 정의

---
+++ src/main/java/com/safecornerscoffee/mocha/mapper/AccountMapper.java
@@ -0,0 +1,20 @@
+package com.safecornerscoffee.mocha.mapper;
+
+import com.safecornerscoffee.mocha.domain.Account;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface AccountMapper {
+
+    Account getAccountById(Long id);
+    Account getAccountByEmail(String email);
+    List<Account> getAccounts();
+
+    void insertAccount(Account account);
+    void updateAccount(Account account);
+    void deleteAccount(Long id);
+}
