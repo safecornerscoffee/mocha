a0b55bfedb9166b46826cdc089af436d8b8a6baa
safecorners <safeconers.mobile@gmail.com>
2021-07-17 23:03:08 +0900

    IndexController > HomeController 변경

--- src/main/java/com/safecornerscoffee/mocha/controller/HomeController.java
+++ src/main/java/com/safecornerscoffee/mocha/controller/HomeController.java
@@ -3,15 +3,14 @@ package com.safecornerscoffee.mocha.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IndexController {
+public class HomeController {
 
     @GetMapping("/")
-    public String indexPage(Model model) {
+    public String home(Model model) {
         model.addAttribute("title", "Mocha");
         model.addAttribute("message", "Hello, Mocha");
-        return "index";
+        return "home";
     }
 }
