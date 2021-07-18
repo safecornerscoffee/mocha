f2b1b70d3808cfce70efb4008ad5c262d57872d8
safecorners <safeconers.mobile@gmail.com>
2021-07-19 03:32:49 +0900

    [#27] POST'/register'

---
+++ src/main/java/com/safecornerscoffee/mocha/controller/RegistrationController.java
@@ -0,0 +1,30 @@
+package com.safecornerscoffee.mocha.controller;
+
+import com.safecornerscoffee.mocha.dto.RegistrationForm;
+import com.safecornerscoffee.mocha.service.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @GetMapping("/register")
+    public String registrationForm(Model model) {
+        model.addAttribute("registrationForm", new RegistrationForm());
+
+        return "registration/registration";
+    }
+
+    @PostMapping("/register")
+    public String signup(RegistrationForm form) {
+        registrationService.register(form);
+
+        return "redirect:/";
+    }
+}
